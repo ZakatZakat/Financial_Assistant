b751053613b37e32a7e3adb2d35adbfdf23cf565
Zakat_Zakat <>
2024-06-12 15:20:13 +0300

    ver_0.01

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, request, jsonify
-#import your_asr_model  # Подразумевается, что модель ASR импортируется здесь
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+# Маршрут для отдачи HTML страницы
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# API для обработки аудио
 @app.route('/api/upload-audio', methods=['POST'])
 def upload_audio():
-    audio_file = request.files['audio']
-    #assert False, (audio_file)
-    #text = your_asr_model.recognize(audio_file)  # Обработка файла аудио вашей моделью ASR
-    text = 'derp_derp_derp'
-    return jsonify(text=text)
+    if 'audio' in request.files:
+        audio_file = request.files['audio']
+        # Обработка файла аудио вашей ASR моделью
+        text = 'Пример распознанного текста'
+        return jsonify(text=text)
+    return jsonify({"error": "No audio file provided"}), 400
+
+# API для обновления текста
+@app.route('/api/upload', methods=['POST'])
+def simple_upload():
+    return jsonify(text='derp_derp_derp')
 
 if __name__ == '__main__':
     app.run(debug=True)
